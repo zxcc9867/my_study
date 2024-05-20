069f0546dcfb44bebdbddb5d1fa8dbac390249f2
wonjin <zxcc9867@naver.com>
2024-05-20 20:06:25 +0900

    5/20 파이썬 분기문

--- python/Python_Grammer.py
+++ python/Python_Grammer.py
@@ -571,9 +571,9 @@ zip과 dict
 사용자로부터 문자 한개를 입력받고, 소문자일 경우 대문자로, 대문자일 경우 소문자로 변경해서 출력해라. 
 """
 
-alpha = input("영문자를 입력해주세요.")
+# alpha = input("영문자를 입력해주세요.")
 
-print(alpha.upper() if alpha.islower() == True else alpha.lower() ) # 삼항 연산자 사용 
+# print(alpha.upper() if alpha.islower() == True else alpha.lower() ) # 삼항 연산자 사용
 
 """
 삼항 연산자 -> 조건이 참일 때 출력 if 조건 else 조건이 거짓일 때의 출력 
@@ -582,3 +582,68 @@ print(alpha.upper() if alpha.islower() == True else alpha.lower() ) # 삼항 연
 """
 5/13 121까지 수행 
 """
+
+"""
+
+124. 사용자로부터 세 개의 숫자를 입력 받은 후, 가장 큰 숫자를 출력하라. 
+
+map 함수 
+
+map(function, iterable)
+
+
+"""
+
+
+# nums = list(map(str,input("숫자를 입력해주세요.").split()))
+
+# print(f"가장 숫자가 높은 숫자는{max(nums)}이고, 가장 낮은 숫자는{min(nums)}입니다.")
+
+
+"""
+주민등록번호를 보고, 남자 / 여자인지 출력 
+"""
+
+# first , second = str(input("주민번호를 입력해주세요. ( 하이푼 포함 )")).split('-')
+
+# [ print("남자입니다.") if int(second[0]) in [1,3] else print("여자입니다.")]
+
+# if int(second[1]) in range(0,9):
+#     print("서울")
+# elif int(second[1]) in range(9,13):
+#     print("부산")
+
+
+"""
+주민 번호 유효성 검사 
+
+주민등록번호는 13자리로 구성되는데 마지막 자리수는 주민등록번호의 유효성을 체크하는데 사용된다. 
+먼저 앞에서 부터 12자리의 숫자에 2,3,4,5,6,7,8,9,2,3,4,5를 차례로 곱한 뒤 그 값을 전부 더하고, 
+연산 결과 값을 11로 나누면 나머지가 나오는데 11에서 나머지를 뺀 값이 주민등록 번호의 마지막 값이 된다. 
+"""
+nums = input("주민번호를 입력하세요. 12자리 (하이픈 포함) : ")
+
+# 하이픈을 기준으로 주민번호를 분리
+first_number, second_number = nums.split("-")
+
+# 하이픈을 제거하고 주민번호를 합침
+full_number = list(first_number + second_number)
+
+print(full_number)
+
+multiple_num = [2,3,4,5,6,7,8,9,2,3,4,5]
+
+# 결과를 저장할 리스트 초기화
+numbers = []
+
+# full_number의 각 자릿수를 순회
+for i in range(0,len(full_number)):
+    # 각 자릿수를 정수로 변환
+    numbers.append(int(full_number[i])*multiple_num[i])
+
+last_num = 11 - sum(numbers)%11
+
+identity_number=nums+str(last_num)
+
+
+print(identity_number)
