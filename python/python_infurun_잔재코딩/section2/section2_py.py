c842acb65cc256f8f22dad408cf8abb5ea826109
wonjin <zxcc9867@naver.com>
2024-08-29 23:55:12 +0900

    반복문 활용2

---
+++ python/python_infurun_잔재코딩/section2/section2_py.py
@@ -0,0 +1,27 @@
+"""
+
+find function
+
+"""
+
+# string = 'find function'
+
+# print(string.find('f',3)) # 3번째 인덱스 뒤에서 부터 f가 나오는 것을 찾는다.
+
+# print(string.find('f', 0, 5)) # 0번째부터 5번째 인덱스 사이에서 f가 나오는 것을 찾는다.
+
+
+"""
+
+example 
+
+"""
+
+# num = input("주민번호를 입력하세요 : ")
+# if int(num.split("-")[1][1:3]) < 9:
+
+#     print("busan")
+# elif int(num.split("-")[1][0:2]) >= 9:
+#     print("seoul")
+
+
