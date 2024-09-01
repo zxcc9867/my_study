6b5dc5c1c88fe7f499634113d7779477bd064404
wonjin <zxcc9867@naver.com>
2024-09-01 23:34:28 +0900

    파이썬 문법 완료 후, 크롤링을 위한 html공부까지 수행

--- python/python_infurun_잔재코딩/section2/section2_py.py
+++ python/python_infurun_잔재코딩/section2/section2_py.py
@@ -24,4 +24,33 @@ example
 # elif int(num.split("-")[1][0:2]) >= 9:
 #     print("seoul")
 
+"""
+
+소인수 분해 
+
+"""
+
+
+def factor_divider(num: int) -> list:
+    factors = []
+    input_num = num  # num to be divided
+    divder = 2
+    while num > 1:
+
+        if num % divder == 0:
+            factors.append(divder)
+            num //= divder
+        else:
+            divder += 1
+            print(divder)
+    return (
+        f"숫자 {input_num} 에서 {factors} 로 소인수분해가 됩니다."
+        if factors
+        else f"{input_num} 에서 소인수분해할 수 없습니다"
+    )
+
+
+print(factor_divider(int(input("숫자를 입력하세요. : "))))
+
+
 
